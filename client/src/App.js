2d425546a90e071d4978246ce3002fd577f365bd
abuhuraira24 <devloperabuhuraira@gmail.com>
2022-04-27 11:52:54 +0600

    start

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+function App() {
+  return (
+    <div>
+      <h2>
+        <Button>Buy Now</Button>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
