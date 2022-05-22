f8da233c03e7cebe8c26fbca2aeaef0709412fd2
abuhuraira24 <devloperabuhuraira@gmail.com>
2022-05-22 15:23:46 +0600

    make it better

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -18,6 +19,12 @@ app.use(passport.initialize())
 require('./passport')(passport)
 
 app.use('/api', require('./router/userAuth'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 4000;
 
