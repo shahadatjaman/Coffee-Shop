524ad33cdae1a4f0ca53509cf5ad94f4b63323c8
abuhuraira24 <devloperabuhuraira@gmail.com>
2022-05-22 17:35:19 +0600

    make it better

--- passport.js
+++ passport.js
@@ -4,11 +4,10 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 
 const User = require('./model/RegisterModel')
 
-const {PRIVET_KEY} = require("./config")
 
 let opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = PRIVET_KEY;
+opts.secretOrKey = "SECRET-KEY";
 
 module.exports = passport => {
     passport.use(new JwtStrategy(opts, (payload, done) => {
